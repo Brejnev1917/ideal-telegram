38b8cd61e93518132f555474e6b24b068228ab1c
Brejnev1917 <91326516+Brejnev1917@users.noreply.github.com>
2021-11-12 11:01:03 +0300

    Add files via upload

--- sqrServiceTest.java
+++ sqrServiceTest.java
@@ -1,13 +1,17 @@
 package ru.netology.sqr;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class sqrServiceTest {
 
-    @Test
-    void shouldCalculateNumberSQRT() {
+    @ParameterizedTest
+    @CsvSource (value = {"'1',3","'2',15","'3',200","'4',201","'5',300","'6',301"})
+    void shouldCalculateNumberSQRT(String testName,int expected) {
         sqrService  sqrService = new sqrService();
-        int expected = 3;
+
         int actual = sqrService.sqrt(200,300);
         assertEquals(expected,actual);
     }
